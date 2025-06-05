5dbbc8ccbb58acf7c8ea74bd21652b0b3223251a
Enos Andrade <enosads@gmail.com>
2025-06-04 21:21:52 -0300

    Add sealed interface and implementation example with sound handling in Kotlin

--- src/aula_28_classes_e_interfaces_seladas/28_ClassesEInterfacesSeladas.kt
+++ src/aula_28_classes_e_interfaces_seladas/28_ClassesEInterfacesSeladas.kt
@@ -1,5 +1,26 @@
 package aula_28_classes_e_interfaces_seladas
 
+sealed interface Animal {
+    data class Dog(val breed: String) : Animal {
+        override fun sound(): String {
+            return "Au!"
+        }
+    }
+
+    data class Cat(val color: String) : Animal {
+        override fun sound(): String {
+            return "Miau!"
+        }
+    }
+    data object Bird : Animal {
+        override fun sound(): String {
+            return "Piu!"
+        }
+    }
+
+    fun sound(): String;
+}
+
 sealed class Result {
     data class Success(val data: String) : Result()
     data class Error(val exception: Exception) : Result()
@@ -29,12 +50,29 @@ object DataBase {
 
 }
 
+fun handleAnimal(animal: Animal){
+    println(when(animal){
+        is Animal.Cat -> "O animal é um gato. O som que ele faz é ${animal.sound()} e sua cor é ${animal.color}"
+        is Animal.Dog -> "O animal é um cachorro. O som que ele faz é ${animal.sound()} e a sua raça é ${animal.breed}"
+        Animal.Bird -> "O animal é um pássaro. O som que ele faz é ${Animal.Bird.sound()}"
+    })
+}
+
+
 fun main() {
     val contacts = DataBase.getContacts()
     val isContactInserted = DataBase.insertContact(contact = "Enos")
     val isContactUpdated = DataBase.updateContact(id = 1, newContact = "Enos")
 
+
     handleResult(result = contacts)
     handleResult(result = isContactInserted)
     handleResult(result = isContactUpdated)
+
+    val dog = Animal.Dog(breed = "Labrador")
+    handleAnimal(animal = dog)
+
+    val bird = Animal.Bird
+    handleAnimal(animal = bird)
+
 }
